79f160d9b7687671bfee18821e0aa170e021ce8d
PhunnieB <phunniehypnotix@gmail.com>
2022-05-08 04:19:00 -0400

    Themes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 
 const themeDark = createTheme({
   palette: {
-    type: 'light',
+    type: 'dark',
     primary: {
       main: '#3f51b5',
     },
@@ -21,8 +21,30 @@ const themeDark = createTheme({
     text: {
       primary: 'rgba(255,255,255,0.85)',
       secondary: 'rgba(232,229,225,0.54)',
-      disabled: 'rgba(214,214,214,0.38)',
+      disasbled: 'rgba(214,214,214,0.38)',
       hint: 'rgba(191,191,191,0.38)',
+    },
+  },
+});
+
+const themeLight = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#3f51b5',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+    background: {
+      default: '#e4e4e4',
+      paper: '#e4e0df',
+    },
+    text: {
+      primary: '#0e0e0e',
+      secondary: 'rgba(39,39,39,0.77)',
+      disabled: 'rgba(45,45,45,0.38)',
+      hint: 'rgba(49,49,49,0.38)',
     },
   },
 });
@@ -31,6 +53,7 @@ function App(){
   //const [light, setLight] = React.useState(false);
   return (
     
+    //have retrievable setting somewhere for mod
     <ThemeProvider theme={themeDark}>
       <CssBaseline />
       <AppRouter/>
